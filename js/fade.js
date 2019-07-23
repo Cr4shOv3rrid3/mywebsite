e1d5557d011ff819e7c1301134bc32457d6ad312
CrashOverride <cr4shov3rrid3@mynetec.com>
2019-07-23 15:04:04 +0200

    Create fade.js

---
+++ js/fade.js
@@ -0,0 +1,15 @@
+$(function() {
+
+  var documentEl = $(document),
+      fadeElem = $('.fade-scroll');
+      
+  documentEl.on('scroll', function() {
+      var currScrollPos = documentEl.scrollTop();
+      
+      fadeElem.each(function() {
+          var $this = $(this),
+              elemOffsetTop = $this.offset()top;
+          if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 -(currScrollPos - elemOffsetTop)/400);
+       )};
+    )};
+ )};
